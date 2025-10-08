978b65ed412b77171bd86ae6ffc87f36ccb000d0
kangmin <codemin1005@gmail.com>
2025-10-08 19:28:11 +0900

    101p

--- volume2/common/util.py
+++ volume2/common/util.py
@@ -39,3 +39,47 @@ def cos_similarity(x, y, eps=1e-8):
     nx = x / np.sqrt(np.sum(x**2) + eps)
     ny = y / np.sqrt(np.sum(y**2) + eps)
     return np.dot(nx, ny)
+
+def most_similar(query, word_to_id, id_to_word, word_matrix, top=5):
+    if query not in word_to_id:
+        print('%s를 찾을 수 없습니다.' % query)
+        return
+    
+    print('\n[query]' + query)
+    query_id = word_to_id[query]
+    query_vec = word_matrix[query_id]
+  
+    vocab_size = len(word_to_id)
+    similarity = np.zeros(vocab_size)
+    for i in range(vocab_size):
+        similarity[i] = cos_similarity(query_vec, word_matrix[i])
+
+    count = 0
+    for i in (-1*similarity).argsort():
+        if id_to_word[i] == query:
+            continue
+        print(' %s: %s' % (id_to_word[i], similarity[i]))
+
+        count+=1
+        if count >= top:
+            return
+        
+
+def ppmi(C, verbose=False, eps=1e-8):
+    M = np.zeros_like(C, dtype=np.float32)
+    N = np.sum(C)
+    S = np.sum(C, axis=0)
+    total = C.shape[0] * C.shape[1]
+    cnt = 0
+
+    for i in range(C.shape[0]):
+        for j in range(C.shape[1]):
+            pmi = np.log2(C[i,j] * N / (S[i] * S[j]) + eps)
+            M[i,j] = max(0, pmi)
+
+        if verbose:
+            cnt+=1
+            if cnt % (total//100 + 1) == 0:
+                print('%.1f%% 완료' % (100*cnt/total))
+
+    return M
