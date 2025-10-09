2c31925604f5e9d0802b6eb89c0b646eec74e77c
kangmin <codemin1005@gmail.com>
2025-10-09 17:23:14 +0900

    106p

--- volume2/ch02/count_method_small.py
+++ volume2/ch02/count_method_small.py
@@ -1,7 +1,7 @@
 import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import numpy as np
-from common.util import preprocess, create_co_matrix, cos_similarity, ppmi
+from common.util import preprocess, create_co_matrix, ppmi
 
 text = 'You say goodbye and I say hello.'
 corpus, word_to_id, id_to_word = preprocess(text)
@@ -9,9 +9,4 @@ vocab_size = len(word_to_id)
 C = create_co_matrix(corpus, vocab_size)
 W = ppmi(C)
 
-np.set_printoptions(precision=3)
-print('co-matrix')  
-print(C)
-print('-'*50)
-print('ppmi')
-print(W) 
+U,S,V = np.linalg.svd(W)
